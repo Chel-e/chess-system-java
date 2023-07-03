7f2a7dede486dc9e70504c4dc4aa1398f5da3a0a
Michel <michel.mf628@gmail.com>
2023-07-03 20:21:28 -0300

    start to implement class Board and Piece

---
+++ src/Boardgame/Board.java
@@ -0,0 +1,13 @@
+package Boardgame;
+
+public class Board {
+    private int row;
+    private int columns;
+    private Piece[][] pieces;
+    public Board(int row, int columns) {
+        this.row = row;
+        this.columns = columns;
+        pieces = new Piece[row][columns];
+    }
+    
+}
